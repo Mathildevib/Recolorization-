85eb4673e62babd77f03007c7e7d991159a31b0c
Mathilde Vibæk <mathildevibaek@hotmail.com>
2025-11-19 17:38:23 +0100

    Refactor U-Net and Discriminator architecture

--- models.py
+++ models.py
@@ -1,105 +1,124 @@
 import torch
 import torch.nn as nn
 
-class DoubleConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
+# U-Net Generator components
+
+class UNetDown(nn.Module):
+    def __init__(self, in_channels, out_channels, norm=True):
         super().__init__()
-        self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+        layers = [
+            nn.Conv2d(in_channels, out_channels, 4, 2, 1, bias=not norm)
+        ]
+        if norm:
+            layers.append(nn.InstanceNorm2d(out_channels))
+        layers.append(nn.LeakyReLU(0.2, True))
+        self.block = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class UNetUp(nn.Module):
+    def __init__(self, in_channels, out_channels, dropout=False):
+        super().__init__()
+        layers = [
+            nn.ConvTranspose2d(in_channels, out_channels, 4, 2, 1, bias=False),
+            nn.InstanceNorm2d(out_channels),
             nn.ReLU(True)
+        ]
+        if dropout:
+            layers.append(nn.Dropout(0.5))
+        self.block = nn.Sequential(*layers)
+
+    def forward(self, x, skip):
+        x = self.block(x)
+        x = torch.cat([x, skip], dim=1)
+        return x
+
+
+
+# Pix2Pix U-Net Generator
+
+
+class UNetGenerator(nn.Module):
+    def __init__(self, input_nc=3, output_nc=3):
+        super().__init__()
+
+        # Downsampling
+        self.d1 = UNetDown(input_nc,   64, norm=False)
+        self.d2 = UNetDown(64,        128)
+        self.d3 = UNetDown(128,       256)
+        self.d4 = UNetDown(256,       512)
+        self.d5 = UNetDown(512,       512)
+        self.d6 = UNetDown(512,       512)
+        self.d7 = UNetDown(512,       512)
+        self.d8 = UNetDown(512,       512, norm=False)
+
+        # Upsampling
+        self.u1 = nn.Sequential(
+            nn.ConvTranspose2d(512, 512, 4, 2, 1, bias=False),
+            nn.InstanceNorm2d(512),
+            nn.ReLU(True),
+            nn.Dropout(0.5)
+        )
+        self.u2 = UNetUp(512 + 512, 512, dropout=True)
+        self.u3 = UNetUp(512 + 512, 512, dropout=True)
+        self.u4 = UNetUp(512 + 512, 512)
+        self.u5 = UNetUp(512 + 512, 256)
+        self.u6 = UNetUp(256 + 256, 128)
+        self.u7 = UNetUp(128 + 128,  64)
+
+        self.final = nn.Sequential(
+            nn.ConvTranspose2d(64 + 64, output_nc, 4, 2, 1),
+            nn.Tanh()
         )
 
     def forward(self, x):
-        return self.double_conv(x)
+        d1 = self.d1(x)
+        d2 = self.d2(d1)
+        d3 = self.d3(d2)
+        d4 = self.d4(d3)
+        d5 = self.d5(d4)
+        d6 = self.d6(d5)
+        d7 = self.d7(d6)
+        d8 = self.d8(d7)
 
+        u1 = self.u1(d8)
+        u1 = torch.cat([u1, d7], dim=1)
 
-class UNet(nn.Module):
-    def __init__(self):
-        super().__init__()
+        u2 = self.u2(u1, d6)
+        u3 = self.u3(u2, d5)
+        u4 = self.u4(u3, d4)
+        u5 = self.u5(u4, d3)
+        u6 = self.u6(u5, d2)
+        u7 = self.u7(u6, d1)
 
-        self.conv1 = DoubleConv(1, 32)
-        self.pool1 = nn.MaxPool2d(2)
+        return self.final(u7)
 
-        self.conv2 = DoubleConv(32, 64)
-        self.pool2 = nn.MaxPool2d(2)
-
-        self.conv3 = DoubleConv(64, 128)
-        self.pool3 = nn.MaxPool2d(2)
-
-        self.conv4 = DoubleConv(128, 256)
-        self.pool4 = nn.MaxPool2d(2)
-
-        self.bottleneck = DoubleConv(256, 512)
-
-        self.transconv1 = nn.ConvTranspose2d(512, 256, 2, 2)
-        self.concat_conv1 = DoubleConv(512, 256)
-
-        self.transconv2 = nn.ConvTranspose2d(256, 128, 2, 2)
-        self.concat_conv2 = DoubleConv(256, 128)
-
-        self.transconv3 = nn.ConvTranspose2d(128, 64, 2, 2)
-        self.concat_conv3 = DoubleConv(128, 64)
-
-        self.transconv4 = nn.ConvTranspose2d(64, 32, 2, 2)
-        self.concat_conv4 = DoubleConv(64, 32)
-
-        self.final_conv = nn.Conv2d(32, 3, kernel_size=1)
-
-    def forward(self, x):
-        enc1 = self.conv1(x)
-        p1 = self.pool1(enc1)
-
-        enc2 = self.conv2(p1)
-        p2 = self.pool2(enc2)
-
-        enc3 = self.conv3(p2)
-        p3 = self.pool3(enc3)
-
-        enc4 = self.conv4(p3)
-        p4 = self.pool4(enc4)
-
-        b = self.bottleneck(p4)
-
-        up1 = self.transconv1(b)
-        dec1 = self.concat_conv1(torch.cat([up1, enc4], dim=1))
-
-        up2 = self.transconv2(dec1)
-        dec2 = self.concat_conv2(torch.cat([up2, enc3], dim=1))
-
-        up3 = self.transconv3(dec2)
-        dec3 = self.concat_conv3(torch.cat([up3, enc2], dim=1))
-
-        up4 = self.transconv4(dec3)
-        dec4 = self.concat_conv4(torch.cat([up4, enc1], dim=1))
-
-        return self.final_conv(dec4)
-
+# PatchGAN Discriminator
 
 class Discriminator(nn.Module):
     def __init__(self):
         super().__init__()
 
-        def conv_block(in_c, out_c, norm=True):
+        def block(in_c, out_c, normalize=True):
             layers = [nn.Conv2d(in_c, out_c, 4, 2, 1)]
-            if norm:
+            if normalize:
                 layers.append(nn.InstanceNorm2d(out_c))
             layers.append(nn.LeakyReLU(0.2, True))
-            return nn.Sequential(*layers)
+            return layers
 
-    
         self.model = nn.Sequential(
-            conv_block(4, 64, norm=False),
-            conv_block(64, 128),
-            conv_block(128, 256),
-            conv_block(256, 512),
-            nn.Conv2d(512, 1, 4, 1, 1)
+            *block(6, 64, normalize=False),
+            *block(64, 128),
+            *block(128, 256),
+            nn.Conv2d(256, 1, 4, 1, 1)
         )
 
-    def forward(self, grey, color):
-        x = torch.cat([grey, color], dim=1)
+    def forward(self, A, B):
+        x = torch.cat([A, B], dim=1)
         return self.model(x)
+
 
 
     
